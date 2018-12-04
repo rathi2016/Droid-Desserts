9e9ffa82d32998b4a7d2dfd4876c576e9e248937
Rathi <rathind14@gmail.com>
2018-12-04 12:01:17 -0500

    included shopping cart floting btn

--- app/src/main/java/com/bequite/rathind/fragreyclerpractice/DessertTime.java
+++ app/src/main/java/com/bequite/rathind/fragreyclerpractice/DessertTime.java
@@ -1,11 +1,12 @@
 package com.bequite.rathind.fragreyclerpractice;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 public class DessertTime extends AppCompatActivity {
 
@@ -20,10 +21,39 @@ public class DessertTime extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(getApplicationContext(), OrderActivity.class);
+                startActivity(intent);
             }
         });
     }
 
+
+
+    /*
+     Displays Toast
+     */
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    /*
+     Shows a message that the donut image was clicked.
+     */
+    public void showDonutOrder(View view) {
+        displayToast(getString(R.string.donut_order_message));
+    }
+
+    /*
+    Shows a message that the ice cream sandwich image was clicked.
+     */
+    public void showIceCreamOrder(View view) {
+        displayToast(getString(R.string.ice_cream_order_message));
+    }
+
+    /*
+    Shows a message that the froyo image was clicked.
+     */
+    public void showFroyoOrder(View view) {
+        displayToast(getString(R.string.froyo_order_message));
+    }
 }
--- app/src/main/java/com/bequite/rathind/fragreyclerpractice/MainActivity.java
+++ app/src/main/java/com/bequite/rathind/fragreyclerpractice/MainActivity.java
@@ -1,47 +1,21 @@
 package com.bequite.rathind.fragreyclerpractice;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity{
-//
-//    private static final String LOG_TAG = MainActivity.class.getSimpleName();
-//    public static  final String EXTRA_MSG = "com.example.android.twoactivities.extra.MESSAGE";
-//    private EditText mMessageEditTxt;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("MainActivity", "onCreate: Started");
+        Intent intent = new Intent(this, DessertTime.class);
+        startActivity(intent);
+        finish();
 
-//
-//        SharedPreferences sharedPreferences = this.getSharedPreferences("com.bequite.rathind.fragreyclerpractice", Context.MODE_PRIVATE);
-//        ArrayList<String> friends = new ArrayList<>();
-//        friends.add("Rathi");
-//        friends.add("Mickey");
-//        friends.add("Sanddy");
-//        Gson gson = new Gson();
-//        String json = gson.toJson(friends);
-//
-//        try{
-//            sharedPreferences.edit().putString("friends",json).apply();
-//            Log.i("friends",json);
-//
-//        }catch(Exception e){
-//             e.printStackTrace();
-//        }
-//        ArrayList<String> newfriends = new ArrayList<>();
-//        Gson newjson = new Gson();
-
-
-
-//   sharedPreferences.edit().putString("userName","r2d2").apply();
-//       String username = sharedPreferences.getString("userName","");
-//       Log.i("this is the username", username);
-//
-//        mMessageEditTxt = findViewById(R.id.textView2);
     }
 
     @Override
@@ -49,11 +23,5 @@ public class MainActivity extends AppCompatActivity{
         super.onStart();
     }
 
-//    public void launchSecondActivity(View view) {
-//        Log.d(LOG_TAG, "Button clicked!");
-//        Intent intent = new Intent(this, SecondActivity.class);
-//        String message = mMessageEditTxt.getText().toString();
-//        intent.putExtra(EXTRA_MSG,message);
-//        startActivity(intent);
-//    }
+
 }
